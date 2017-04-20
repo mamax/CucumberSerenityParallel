0765e8bee4ef95cb61737af3d021f38692da9000
Maksym Mazurkevych <maksym_mazurkevych@epam.com>
2017-04-20 16:24:41 +0300

    'Added_parallel_plugin_for_execution'

--- src/test/java/com/cucumber/serenity/DefinitionTestSuite.java
+++ src/test/java/com/cucumber/serenity/DefinitionTestSuite.java
@@ -5,5 +5,5 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features={"src/test/resources/features/consult_dictionary/LookupADefinition.feature", "src/test/resources/features/consult_dictionary/LookupADefinition_1.feature", "src/test/resources/features/consult_dictionary/LookupADefinition_2.feature", "src/test/resources/features/consult_dictionary/LookupADefinition_3.feature"})
+@CucumberOptions(features={"src/test/resources/features/LookupADefinition.feature", "src/test/resources/features/LookupBDefinition.feature", "src/test/resources/features/LookupCDefinition.feature", "src/test/resources/features/LookupDDefinition.feature"})
 public class DefinitionTestSuite {}
