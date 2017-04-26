293baa65374f492ccc13fc468550a4d1330d97af
Maksym Mazurkevych <maksym_mazurkevych@epam.com>
2017-04-26 11:34:37 +0300

    Features_all_in_suite

--- src/test/java/com/cucumber/serenity/DefinitionTestSuite.java
+++ src/test/java/com/cucumber/serenity/DefinitionTestSuite.java
@@ -5,5 +5,5 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features={"src/test/resources/features/LookupADefinition.feature", "src/test/resources/features/LookupBDefinition.feature", "src/test/resources/features/LookupCDefinition.feature", "src/test/resources/features/LookupDDefinition.feature"})
+@CucumberOptions(features = "src/test/resources/features/")
 public class DefinitionTestSuite {}
