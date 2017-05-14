308dd88c89c27395e281a920423f08b041204aaf
Maksym Mazurkevych <maksym_mazurkevych@epam.com>
2017-05-14 21:53:22 +0300

    Update pom and DefinitionTestSuite

--- src/test/java/com/cucumber/serenity/DefinitionTestSuite.java
+++ src/test/java/com/cucumber/serenity/DefinitionTestSuite.java
@@ -5,5 +5,5 @@ import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
 
 @RunWith(CucumberWithSerenity.class)
-@CucumberOptions(features={"src/test/resources/features/Lookup_A_Definition.feature", "src/test/resources/features/Lookup_B_Definition.feature", "src/test/resources/features/Lookup_C_Definition.feature", "src/test/resources/features/Lookup_D_Definition.feature"})
+@CucumberOptions(features="src/test/resources/features")
 public class DefinitionTestSuite {}
