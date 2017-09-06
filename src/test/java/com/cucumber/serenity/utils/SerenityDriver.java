18bdd5ca3c4152d08f54cd3869969cb76955ddf7
Maksym Mazurkevych <maksym_mazurkevych@epam.com>
2017-09-06 09:33:32 +0300

    Updated_reportportal_dependency

---
+++ src/test/java/com/cucumber/serenity/utils/SerenityDriver.java
@@ -0,0 +1,24 @@
+//package com.cucumber.serenity.utils;
+//
+//import io.github.bonigarcia.wdm.ChromeDriverManager;
+//import net.thucydides.core.webdriver.DriverSource;
+//import org.openqa.selenium.WebDriver;
+//import org.openqa.selenium.chrome.ChromeDriver;
+//
+///**
+// * Created by Maksym_Mazurkevych on 7/3/2017.
+// */
+//public class SerenityDriver implements DriverSource {
+//
+//
+//    @Override
+//    public WebDriver newDriver() {
+//        ChromeDriverManager.getInstance().setup();
+//        return new ChromeDriver();
+//    }
+//
+//    @Override
+//    public boolean takesScreenshots() {
+//        return false;
+//    }
+//}
