9756f5ed49ada38eeb2c41784bab73d2fe0b4db7
Maksym Mazurkevych <maksym.mazurkevych@ticketmaster.com>
2018-01-18 15:54:07 +0200

    Added possibility to run chromedriver on different os

---
+++ README.md
@@ -0,0 +1,7 @@
+**Cucumber test automation project**
+===================================
+
+To run automation tests according to environments:
+ - **windows** : `mvn clean verify -Dos=windows`
+ - **mac** : `mvn clean verify -Dos=osx`
+ - **linux** :`mvn clean verify -Dos=linux`
--- src/test/java/com/cucumber/serenity/DefinitionTestSuite.java
+++ src/test/java/com/cucumber/serenity/DefinitionTestSuite.java
@@ -2,8 +2,24 @@ package com.cucumber.serenity;
 
 import cucumber.api.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.BeforeClass;
 import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(features="src/test/resources/features")
-public class DefinitionTestSuite {}
+public class DefinitionTestSuite {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DefinitionTestSuite.class.getSimpleName());
+
+    @BeforeClass
+    public static void setUpProperties() {
+        try {
+            Class.forName("com.cucumber.serenity.utils.SerenityProperties");
+        } catch (ClassNotFoundException e) {
+            LOG.error("Error instantiating properties", e);
+        }
+    }
+
+}
